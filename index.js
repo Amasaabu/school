95d7bccf124dfbcfd471185b3ff62c0814abfc69
Abubakar Amasa <amasaabubakar@gmail.com>
2021-03-07 07:42:26 +0100

    init release v-2

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use('/session', Session)
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV==='production') {
-    app.use(express.static(path.join(__dirname, '../front/build/')))
+    app.use(express.static(path.join(__dirname, '/front/build/')))
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, '../front', 'build', 'index.html' ))
+        res.sendFile(path.resolve(__dirname, '/front', 'build', 'index.html' ))
     })
 }
 
